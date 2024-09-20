7c10ba5672fc905410337c15dcab9ee8ccdcb398
Aloizio Martins <martins.aloizio@gmail.com>
2024-09-20 17:36:50 -0300

    estrutura e templates

--- recipes/views.py
+++ recipes/views.py
@@ -4,8 +4,16 @@ from django.http import HttpResponse
 # Create your views here.
 
 
+
 def home(request):
-    return HttpResponse("Página inicial recipes")
+    return render(request, 'recipes/home.html', status=200, context={
+        'name' : 'Aloizio Martins'
+
+    })
+
+
+#def home(request):
+#    return HttpResponse("Página inicial recipes")
 
 def contato(request):
     return HttpResponse("Contato recipes ")
