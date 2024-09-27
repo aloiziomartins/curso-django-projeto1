6a2887a13a127c13480057a014974af96dafa60a
Aloizio Martins <martins.aloizio@gmail.com>
2024-09-27 12:12:06 -0300

    erro

--- recipes/views.py
+++ recipes/views.py
@@ -7,3 +7,8 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'recipes/pages/home.html')
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html')
+
